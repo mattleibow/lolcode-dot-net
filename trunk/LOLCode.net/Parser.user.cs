fd25036db17ca65180a3cc727397f17d5390d731
arachnidster <arachnidster@5688aeef-c931-0410-91e0-437e01270c33>
2007-06-06 08:56:29 +0000

    Improved string constant parsing to support escape sequences.

--- trunk/LOLCode.net/Parser.user.cs
+++ trunk/LOLCode.net/Parser.user.cs
@@ -52,7 +52,7 @@ namespace notdot.LOLCode
 
         void Error(string s)
         {
-            if (errDist >= minErrDist) errors.SemErr(filename, la.line, la.col, s);
+            if (errDist >= minErrDist) errors.SemErr(filename, t.line, t.col, s);
             errDist = 0;
         }
 
@@ -87,6 +87,97 @@ namespace notdot.LOLCode
         {
             if (!locals.Contains(name))
                 Error(string.Format("Reference to undefined symbol \"{0}\"", name));
+        }
+
+        private string UnescapeString(string str)
+        {
+            int lastIdx = 1;
+            int idx;
+            StringBuilder ret = new StringBuilder();
+
+            try
+            {
+                while ((idx = str.IndexOf('\\', lastIdx)) != -1)
+                {
+                    //Append the string between the last escape and this one
+                    ret.Append(str, lastIdx, idx - lastIdx);
+
+                    //Decipher the escape
+                    switch (str[idx + 1])
+                    {
+                        case '\'':
+                            ret.Append('\'');
+                            lastIdx = idx + 2;
+                            break;
+                        case '"':
+                            ret.Append('"');
+                            lastIdx = idx + 2;
+                            break;
+                        case '\\':
+                            ret.Append('\\');
+                            lastIdx = idx + 2;
+                            break;
+                        case '0':
+                            ret.Append('\0');
+                            lastIdx = idx + 2;
+                            break;
+                        case 'a':
+                            ret.Append('\a');
+                            lastIdx = idx + 2;
+                            break;
+                        case 'b':
+                            ret.Append('\b');
+                            lastIdx = idx + 2;
+                            break;
+                        case 'f':
+                            ret.Append('\f');
+                            lastIdx = idx + 2;
+                            break;
+                        case 'n':
+                            ret.Append('\n');
+                            lastIdx = idx + 2;
+                            break;
+                        case 'r':
+                            ret.Append('\r');
+                            lastIdx = idx + 2;
+                            break;
+                        case 't':
+                            ret.Append('\t');
+                            lastIdx = idx + 2;
+                            break;
+                        case 'v':
+                            ret.Append('\v');
+                            lastIdx = idx + 2;
+                            break;
+                        case 'x':
+                            int end = idx + 3;
+                            while ("0123456789abcdefABCDEF".IndexOf(str[end]) != -1)
+                                end++;
+                            if (end - idx > 6)
+                                end = idx + 6;
+                            ret.Append(char.ConvertFromUtf32(int.Parse(str.Substring(idx + 2, end - idx - 2), System.Globalization.NumberStyles.AllowHexSpecifier)));
+                            lastIdx = end;
+                            break;
+                        case 'u':
+                            ret.Append(char.ConvertFromUtf32(int.Parse(str.Substring(idx + 2, 4), System.Globalization.NumberStyles.AllowHexSpecifier)));
+                            lastIdx = idx + 6;
+                            break;
+                        case 'U':
+                            ret.Append(char.ConvertFromUtf32(int.Parse(str.Substring(idx + 2, 8), System.Globalization.NumberStyles.AllowHexSpecifier)));
+                            lastIdx = idx + 10;
+                            break;
+                    }
+                }
+
+                //Append the end of the string
+                ret.Append(str, lastIdx, str.Length - lastIdx - 1);
+            }
+            catch (Exception ex)
+            {
+                SemErr(string.Format("Invalid escape sequence in string constant: {0}", ex.Message));
+            }
+
+            return ret.ToString();
         }
     }
 }
--- trunk/lolc/test.lol
+++ trunk/lolc/test.lol
@@ -1,7 +1,4 @@
 HAI
-	I HAS A VAR3. LOL VAR3 R 10
-	I HAS A VAR4. LOL VAR4 R 20
-	
-    BTW VISIBLE can also be used to print expressions
-    VISIBLE VAR3 UP VAR4 OVAR 2
+	I HAS A VAR
+	LOL VAR R "Hello\tfoo\nBar\xaboop\x1ffoo\x1fffblarg\u1234florg\U00011111fnorgle"
 KTHXBYE
